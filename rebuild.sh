489b369525f242195db0dbac2cb0529d4e7aa02a
Aurelio Lopez-Fernandez <alopfer1@upo.es>
2023-11-27 11:24:24 +0100

    v0.1.1 created

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@ python3 setup.py sdist
 python3 -m build
 
 # Local installation
-python3 -m pip install ./dist/bioscience-0.1.0.tar.gz
+python3 -m pip install ./dist/bioscience-0.1.1.tar.gz
 
 # Upload to PyPi repository
 #python3 -m twine upload --repository bioscience dist/*
